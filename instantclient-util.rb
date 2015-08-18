507a9aeda5f58847d86b265765501d47b8c5eed0
Linas Juškevičius <linas@prodigito.lt>
2015-08-18 19:09:38 +0300

    Adhere to Rubocop guide.

--- instantclient-util.rb
+++ instantclient-util.rb
@@ -15,7 +15,7 @@ module InstantclientUtil
     file = Pathname.new(file)
     file.ensure_writable do
       file.dynamically_linked_libraries.each do |fname|
-        next if fname[0] == '@'
+        next if fname[0] == "@"
         if ORACLE_LIBRARIES.include? File.basename(fname)
           system MacOS.locate("install_name_tool"), "-change", fname,
                  HOMEBREW_PREFIX/"lib"/File.basename(fname), file
