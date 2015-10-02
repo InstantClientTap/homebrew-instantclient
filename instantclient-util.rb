9c4ebf0031994fbb1bd9b5a0b42abd5537e3d0e8
Kubo Takehiro <kubo@jiubao.org>
2015-10-03 00:49:34 +0900

    Fix dependent Oracle library paths embedded in Oracle libraries again.

--- instantclient-util.rb
+++ instantclient-util.rb
@@ -14,6 +14,10 @@ module InstantclientUtil
   def fix_oracle_lib_path(file)
     file = Pathname.new(file)
     file.ensure_writable do
+      if file.basename.to_s == ORACLE_LIBRARIES[0] # libclntsh.dylib
+        system MacOS.locate("install_name_tool"), "-add_rpath",
+                 HOMEBREW_PREFIX/"lib", file
+      end
       file.dynamically_linked_libraries.each do |fname|
         next if fname[0] == "@"
         if ORACLE_LIBRARIES.include? File.basename(fname)
