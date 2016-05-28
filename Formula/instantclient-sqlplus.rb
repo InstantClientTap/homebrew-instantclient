459dacaf806738dd1625c18191fff8f4d8dab93a
Kubo Takehiro <kubo@jiubao.org>
2016-05-28 11:17:53 +0900

    Oracle instant client 12.1.0.2

--- Formula/instantclient-sqlplus.rb
+++ Formula/instantclient-sqlplus.rb
@@ -1,27 +1,23 @@
 require File.expand_path("../../Strategies/cache_wo_download", __FILE__)
-require File.expand_path("../../instantclient-util", __FILE__)
 
 # A formula that installs the Instant Client SQLPlus package.
 class InstantclientSqlplus < Formula
   desc "Oracle Instant Client SQLPlus x64."
   homepage "http://www.oracle.com/technetwork/topics/intel-macsoft-096467.html"
 
-  url "http://download.oracle.com/otn/mac/instantclient/11204/instantclient-sqlplus-macos.x64-11.2.0.4.0.zip",
+  url "http://download.oracle.com/otn/mac/instantclient/121020/instantclient-sqlplus-macos.x64-12.1.0.2.0.zip",
       :using => CacheWoDownloadStrategy
-  sha256 "127d2baaa4c72d8591af829f00dea5e2a77c0e272ce8fc091dd853e9406845b9"
+  sha256 "8eb5bc4da372c6d58de3b3af30a5736a392278272ea04d4400e0b3926a9bf6b8"
 
   option "with-basiclite", "Depend on instantclient-basiclite instead of instantclient-basic."
 
   depends_on "instantclient-basic" unless build.with?("basiclite")
   depends_on "instantclient-basiclite" if build.with?("basiclite")
 
-  include InstantclientUtil
-
   def install
-    Dir["*.dylib"].each do |file|
-      fix_oracle_lib_path(file)
+    if HOMEBREW_PREFIX.to_s != "/usr/local"
+      system MacOS.locate("install_name_tool"), "-add_rpath", HOMEBREW_PREFIX/"lib", "sqlplus"
     end
-    fix_oracle_lib_path("sqlplus")
     lib.install Dir["*.dylib"]
     bin.install ["sqlplus"]
   end
