99f5e392024bcba2280ecd09d09e638a5b42acc9
Jakob Kofoed Janot <jakob@janot.dk>
2021-01-07 18:34:36 +0100

    Upgrade to Oracle instant client 19.8.0.0.0 (#35)

--- Formula/instantclient-basic.rb
+++ Formula/instantclient-basic.rb
@@ -3,8 +3,8 @@ class InstantclientBasic < Formula
   desc "Oracle Instant Client Basic x64"
   homepage "https://www.oracle.com/database/technologies/instant-client/macos-intel-x86-downloads.html"
 
-  url "https://download.oracle.com/otn_software/mac/instantclient/193000/instantclient-basic-macos.x64-19.3.0.0.0dbru.zip"
-  sha256 "f4335c1d53e8188a3a8cdfb97494ff87c4d0f481309284cf086dc64080a60abd"
+  url "https://download.oracle.com/otn_software/mac/instantclient/198000/instantclient-basic-macos.x64-19.8.0.0.0dbru.zip"
+  sha256 "57ed4198f3a10d83cd5ddc2472c058d4c3b0b786246baebf6bbfc7391cc12087"
 
   conflicts_with "instantclient-basiclite"
 
